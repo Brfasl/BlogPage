71bcd26b030d1d579ffa7492bc0502d5e7e9188f
Brfasl <berfin1884@gmail.com>
2025-02-14 02:53:19 +0300

    Refactor: Improve code structure, fix minor issues & implement new features

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+// test rota
+Route::get('/test',function (){
+    return view('panel.layout.app');
+});

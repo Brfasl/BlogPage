6d014368c6360ec20d5134e745342e8f4068c637
Brfasl <berfin1884@gmail.com>
2025-03-04 21:32:00 +0300

    .

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Livewire\WithPagination;
+
+class HomeController extends Controller
+{
+
+    use WithPagination;
+    /**
+     * Handle the incoming request.
+     */
+    public function __invoke(Request $request)
+    {
+
+    }
+}

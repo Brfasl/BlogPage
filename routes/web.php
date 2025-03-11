e9bd62868f2a29ce8ffc4816b97cbab4b387d83b
Brfasl <berfin1884@gmail.com>
2025-03-11 16:11:17 +0300

    refactor: remove obsolete public panel assets and models
    - chore: delete deprecated models (Category.php, Task.php, User.php)
    - cleanup: remove unused CSS, JS, and image assets
    - build: optimize project by removing redundant vendor libraries and fonts
    
    This commit enhances maintainability by eliminating unused resources and improving repository hygiene.

--- routes/web.php
+++ routes/web.php
@@ -12,17 +12,14 @@ use Illuminate\Support\Facades\Route;
 | Here is where you can register web routes for your application. These
 | routes are loaded by the RouteServiceProvider and all of them will
 | be assigned to the "web" middleware group. Make something great!
-|
+
 */
 
 //test
-Route::get('/test', function () {
+Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/', function () {
-    return view('panel.homepage');
-});
 
 
 Route::group(["prefix" => "admin"], function () {

fd77c46d250e17a0ed11b00513d1d63bf5bd7d34
Brfasl <berfin1884@gmail.com>
2025-03-15 19:11:49 +0300

    feat: Update home view and add HomeController

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -18,29 +18,7 @@
         <div class="mb-16">
             <h2 class="mt-16 mb-5 text-3xl text-yellow-500 font-bold">Featured Posts</h2>
             <div class="w-full">
-                <div class="grid grid-cols-3 gap-10 w-full">
-                    <div class="md:col-span-1 col-span-3">
-                        <a href="http://127.0.0.1:8000/blog/laravel-34">
-                            <div>
-                                <img class="w-full rounded-xl"
-                                     src="http://127.0.0.1:8000/storage/3i5uKG05UnvhbORZ3ieDkvtAOL8ss5-metaZXAxNSAoMjIpLnBuZw==-.png">
-                            </div>
-                        </a>
-                        <div class="mt-3">
-                            <div class="flex items-center mb-2">
-                                <a href="http://127.0.0.1:8000/categories/laravel" class="bg-red-600
-                                        text-white
-                                        rounded-xl px-3 py-1 text-sm mr-3">
-                                    Laravel
-                                </a>
-                                <p class="text-gray-500 text-sm">2023-09-05</p>
-                            </div>
-                            <a class="text-xl font-bold text-gray-900">Laravel 10 tutorial feed page #34</a>
-                        </div>
 
-                    </div>
-
-                </div>
             </div>
             <a class="mt-10 block text-center text-lg text-yellow-500 font-semibold"
                href="http://127.0.0.1:8000/blog">More

470a8d82fdd3f2a279c9dab442c3a1b485de8f86
Brfasl <225541068@firat.edu.tr>
2025-03-20 13:25:23 +0300

    fix: Update home page content

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -13,69 +13,19 @@
         </div>>
     @endsection
 
-        <div class="mb-10">
+        <div class="mb-10 w-full">
             <div class="mb-16">
                 <h2 class="mt-16 mb-5 text-3xl text-yellow-500 font-bold">Featured Posts</h2>
                 <div class="w-full">
 
                 @foreach($featuredPosts as $post)
+                    <div class="md:col-span-1 col-span-3">
 
-                        <div class="md:col-span-1 col-span-3">
-                            <a href="http://127.0.0.1:8000/blog/laravel-34">
-                                <div>
-                                    <img class="w-full rounded-xl"
-                                         src="http://127.0.0.1:8000/storage/3i5uKG05UnvhbORZ3ieDkvtAOL8ss5-metaZXAxNSAoMjIpLnBuZw==-.png">
-                                </div>
-                            </a>
-                            <div class="mt-3">
-                                <div class="flex items-center mb-2">
-                                    <a href="http://127.0.0.1:8000/categories/laravel" class="bg-red-600
-                                        text-white
-                                        rounded-xl px-3 py-1 text-sm mr-3">
-                                        Laravel
-                                    </a>
-                                    <p class="text-gray-500 text-sm">2023-09-05</p>
-                                </div>
-                                <a class="text-xl font-bold text-gray-900">Laravel 10 tutorial feed page #34</a>
-                            </div>
-
-                        </div>
-                        <div class="md:col-span-1 col-span-3">
-                            <a href="http://127.0.0.1:8000/blog/fil3tutorial">
-                                <div>
-                                    <img class="w-full rounded-xl"
-                                         src="http://127.0.0.1:8000/storage/4sEsCDleYEXT4GC7AdU8BP7TBab3cx-metaZmlsYW1lbnQgY291cnNlICg0KS5wbmc=-.png">
-                                </div>
-                            </a>
-                            <div class="mt-3">
-                                <div class="flex items-center mb-2">
-                                    <a href="http://127.0.0.1:8000/categories/PHP" class="bg-blue-400
-                                        text-white
-                                        rounded-xl px-3 py-1 text-sm mr-3">
-                                        PHP</a>
-                                    <p class="text-gray-500 text-sm">2023-09-04</p>
-                                </div>
-                                <a class="text-xl font-bold text-gray-900">Filament 3 relationship manager tutorial
-                                </a>
-                            </div>
-
-                        </div>
-                        <div class="md:col-span-1 col-span-3">
-
-                            <div>
-                                <img class="w-full rounded-xl"
-                                     src="https://via.placeholder.com/640x480.png/0000ee?text=corrupti">
-                            </div>
-                            <div class="mt-3">
-                                <div class="flex items-center mb-2">
-                                    <p class="text-gray-500 text-sm">2023-08-29</p>
-                                </div>
-                                <a class="text-xl font-bold text-gray-900">Mary Berge</a>
-                            </div>
-                        </div>
+                    </div>
+                    @endforeach
                 </div>
             </div>
-                    @endforeach
+
                             </div>
                             <a class="mt-10 block text-center text-lg text-yellow-500 font-semibold"
                                href="http://127.0.0.1:8000/blog">More
